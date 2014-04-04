0f91c0c237dd46d2b5c466d0c7c5ee34f7e23ebd
Romain Quilliot <romain.addweb@gmail.com>
2014-04-04 11:12:08 +0200

    Ajout des notifications

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,36 @@
 $(document).ready(function() {
-   
+    
+    // Affiche le menu
     $('#display-nav').on('click', function () {
         $('#nav, #header, #content').toggleClass('active');
+    });
+
+    // Affiche les notifications
+    $('#display-notif').on('click', function() {
+        $('#notifications').addClass('active');
+        $('#notifications').removeClass('hide');
+        $(this).hide();
+        $('#hide-notif').show();
+    });
+
+    // Cache les notifications
+    $('#hide-notif').on('click', function() {
+        $('#notifications').addClass('hide');
+        $('#notifications').removeClass('active');
+        $(this).hide();
+        $('#display-notif').show();
+    });
+
+    // Animation a la suppression
+    $('.delete-item').on('click', function () {
+        var $current = $(this);
+        var $item = $(this).parent();
+        var $list = $item.parent();
+
+        $item.addClass('animated slideOutLeft');
+        setTimeout(function() {
+            $item.remove();
+        }, 500);
     });
 
 });
