15d9f0c6d2ddceb6ab37cd81a81671ab392aa792
Romain Quilliot <romain.addweb@gmail.com>
2014-04-11 10:57:50 +0200

    Ajout du retour sur les configurations

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,79 @@
+var currentPage = "dashboard/index";
+var oldPage = "";
+
+var app = {
+    init: function () {
+        Template.load('dashboard/index', $('#content'));
+    }
+};
+
+var UI = {
+    showBack: function () {
+        $('#display-nav').hide();
+        $('#back').addClass('active');
+    },
+
+    hideBack: function () {
+        $('#back').removeClass('active');
+        $('#display-nav').show();
+    }
+};
+
+var Template = {
+    load: function (page, $content) {
+        oldPage = currentPage;
+        currentPage = page;
+
+        $.get('views/'+ page +'.html', function (result) {
+            $content.html(result);
+        })
+            .error(function () {
+                var errorTemplate = $('#template-error').html();
+                $('#content').html(errorTemplate);
+            });
+    }
+};
+
 $(document).ready(function() {
+
+    app.init();
     
+    // Systeme de changement de page
+    $('body').on('click', '.change-page', function (event) {
+        event.preventDefault();
+        var page = $(this).attr('data-page');
+        var $content = $('#content');
+
+        Template.load(page, $content);
+    });
+
+    // Selectionne les tabs du menu
+    $('#nav').on('click', 'h3', function () {
+        $('#nav h3.active').removeClass('active');
+        $(this).addClass('active');
+    });
+
     // Affiche le menu
     $('#display-nav').on('click', function () {
         $('#nav, #header, #content').toggleClass('active');
     });
 
+    // Affiche les notifs
+    $('#display-notif').on('click', function() {
+        $(this).hide();
+        $('#hide-notif').show();
+    });
+
+    // Cache les notifs
+    $('#hide-notif').on('click', function() {
+        $(this).hide();
+        $('#display-notif').show();
+
+        Template.load(oldPage, $('#content'));
+    });
+
     // Animation a la suppression
-    $('.delete-item').on('click', function () {
+    $('#content').on('click', '.delete-item', function () {
         var $current = $(this);
         var $item = $(this).parent();
         var $list = $item.parent();
@@ -21,6 +88,22 @@ $(document).ready(function() {
                 $list.html('<br><center>Aucune notification</center>');
             }
         }, 600);
+    });
+
+    // Back
+    $('#back').on('click', function() {
+        var hasClass = $(this).hasClass('active');
+
+        if (hasClass == true) {
+            Template.load(oldPage, $('#content'));
+            UI.hideBack();
+        } else {
+            UI.showBack();
+        }
+    });
+
+    $('body').on('click', '.disp-back', function() {
+        UI.showBack();
     });
 
 });
