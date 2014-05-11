c3a2b4e25cb3fd4e91d291dbffdeab8012128e09
Romain Quilliot <romain.addweb@gmail.com>
2014-05-11 11:20:31 +0200

    Ajout du controller de connexion

---
+++ app/scripts/controllers/loginCtrl.js
@@ -0,0 +1,57 @@
+var loginCtrl = {
+
+    isConnected: function (success, error)
+    {
+        if (!localStorage.token) {
+            error();
+            return false;
+        }
+
+        var token = localStorage.token;
+
+        if (token == null) {
+            localStorage.token = '';
+            error();
+            return false;
+        }
+
+        apiCtrl.request('/token/verify', {
+            method: 'POST',
+            data: {
+                token: token
+            }
+        }, function (result) {
+            if (result.error) {
+                localStorage.token = '';
+                error();
+                return false;
+            }
+
+            success();
+        });
+    },
+
+    isAutorized: function (password, success, error)
+    {
+        if (password == null) {
+            error();
+            return false;
+        }
+
+        apiCtrl.request('/token/request', {
+            method: 'POST',
+            data: {
+                password: password
+            }
+        }, function (result) {
+            if (result.error) {
+                error();
+                return false;
+            }
+
+            localStorage.token = result.token;
+            success();
+        });
+    }
+
+};
