7d3c5d3ec8ab9b9769628740fbcd4669e9a909fd
Romain Quilliot <romain.addweb@gmail.com>
2014-05-11 11:20:40 +0200

    Ajout du model index

---
+++ app/scripts/models/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+    loginCtrl.isConnected(
+        // Si il est connecte
+        function () {
+            window.location.href = 'dashboard.html';
+        },
+        // Si il est deconnecte
+        function () { }
+    );
+
+    $('#login').on('submit', function (event) {
+        event.preventDefault();
+        var password = $('#password').val();
+
+        loginCtrl.isAutorized(password,
+            // En cas de succes
+            function () {
+                window.location.href = 'dashboard.html';
+            },
+            // En cas d'erreur
+            function () {
+                alert('Mot de passe invalide');
+            }
+        );
+    });
+
+});
